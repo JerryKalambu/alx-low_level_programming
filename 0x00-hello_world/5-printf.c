0196f9b18c84d7015a2c48654db0d8c59a2b1cf6
JerryKalambu <jeroldkalambu@gmail.com>
2022-09-08 16:10:00 -0700

    My first C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print a text
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but theoutcome is a piece of art,\n");
+	return(0)
+}
