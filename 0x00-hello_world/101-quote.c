70edadad3ed7264f619f831f45341b7d8f0e0da5
JerryKalambu <jeroldkalambu@gmail.com>
2022-09-08 16:36:43 -0700

    Unix is basically simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<unstd.h>
+#include<unistd.h>
 
 /**
  * main - Print
@@ -9,5 +9,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 }
