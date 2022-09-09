e32c91005fbbcf469076539fe8188c115a949974
JerryKalambu <jeroldkalambu@gmail.com>
2022-09-09 14:19:24 -0700

    2 digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,12 +12,12 @@ int main(void)
 {
 	int n1 =0, n2;
 
-	while (n1 <= 99)
+	while(n1 <= 99)
 	{
 		n2 = n1;
-		while (n2 <= 99)
+		while(n2 <= 99)
 		{
-			if (n2 != n1)
+			if(n2 != n1)
 			{
 				putchar((n1 / 10) + 48);
 				putchar((n1 % 10) + 48);
@@ -31,10 +31,10 @@ int main(void)
 					putchar(' ');
 				}
 			}
-			++n1;
+			++n2;
 		}
-		++n2;
+		++n1;
 	}
-	putchar ('\n');
-	return (0);
+	putchar('\n');
+	return(0);
 }
