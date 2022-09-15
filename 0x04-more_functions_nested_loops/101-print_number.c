5c851fa3369117f1b2867f78e19724c7a5b37825
JerryKalambu <jeroldkalambu@gmail.com>
2022-09-15 16:52:02 -0700

    life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,18 @@
  * Return: always 0
  */ 
  
-void print_number(int n) 
-{ 
-unsigned int x = n; 
- 
-if (n < 0) 
-{ 
-putchar(45); 
-x = -x; 
-} 
-if (x > 9) 
-print_number(x / 10); 
-_putchar((x % 10) + '0'); 
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	putchar(num % 10 + '0');
 }
