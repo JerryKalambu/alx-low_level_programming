a00bc3d5fafcfb2103a09308ce9ce3a5692f7f01
JerryKalambu <jeroldkalambu@gmail.com>
2022-09-30 18:31:44 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print the name of the program
+ * @argc: count arguments
+ * @argv: argument
+ *
+ * return: always 0
+ */
+
+int main(int argc, char *[])
+{
+	(void)argv, /*Ignore argv*/
+	printf("%i\n", argc - 1)'
+	return (0);
+}
