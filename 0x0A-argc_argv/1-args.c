7a0510aa2430735c204e0b8ef5f78c39fe3fdd4a
JerryKalambu <jeroldkalambu@gmail.com>
2022-09-30 18:37:19 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
  * return: always 0
  */
 
-int main(int argc, char *[])
+int main(int argc, char *argv[])
 {
 	(void)argv; /*Ignore argv*/
-	printf("%i\n", argc - 1)
+	printf("%d\n", argc - 1);
 	return (0);
 }
