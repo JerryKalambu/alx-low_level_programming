cc4d04b80652990efa418cfaaefd0f5abaa6a7f8
JerryKalambu <jeroldkalambu@gmail.com>
2022-10-06 17:37:35 -0700

    Task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,32 +9,29 @@
 int _strlen(char *string)
 {
 	int i;
-	
 	for (i = 0; string[i] != '\0'; i++)
-	
+
 	return (i);
 }
 
 /**
- * _strlen_nconcat  - concatenate s1 and n bytes of s2; return ptr to string
+ * _strlen_nconcat - concatenate s1 and n bytes of s2; return ptr to string
  * @s1: string 1
  * @s2: string 2
  * @n: n bytes to concat from string 2
  * Return: pointer to conatenated string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 	char *ptr;
-	int num. len, i,j;
+	int num, len, i, j;
 
 	num = n;
-
-	if (s1 ==NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = ""
+		s2 = "";
 	if (num < 0)
 		return (NULL);
 	if (num >= _Strlen(s2))
@@ -47,9 +44,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j < num; j++
-		ptr[i +j] = s2[j];
-	ptr[i +j] = '\0';
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
 	return (ptr);
 }
