38bf07231aeab7e6b6063c9f63710eea9e51591f
JerryKalambu <jeroldkalambu@gmail.com>
2022-10-27 14:49:21 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,56 +1,22 @@
 #include "main.h"
 
-#define CHAR_BITS 8 /*size of char*/
-#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
-
 /**
- * print_bin - prints binary rep of @n
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another
+ * @n: number one.
+ * @m: number two.
  *
- * @number: decimal value
- *
- * Return: nothing
-*/
-void print_bin(unsigned long int number)
+ * Return: number of bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	if (number >> 1)
-		print_bin(number >> 1);
+	unsigned int nbits;
 
-	putc((number & 1) ? '1' : '0', stdout);
-}
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbits++;
+	}
 
-/**
- * clear_bit - sets the value of a bit to 1 at a given
- *           index.
- *
- * @n: number
- * @index: index to set the value of bit to 1
- *
- * Return: 1 if it worked OR -1 if it failed
-*/
-int clear_bit(unsigned long int *n, unsigned int index)
-{
-	unsigned long int mask = 1;
-	#ifdef DEBUG
-	unsigned long int n_before = *n;
-	#endif
-
-	/*if index is greater than size of n in binary coded decimal*/
-	if (index > INT_BITS)
-		return (-1);
-
-	mask = ~(mask << index); /*create mask based on index position*/
-
-	*n = (*n & mask);
-
-	#ifdef DEBUG
-	printf("\n%ld in binary is ", n_before);
-	print_bin(n_before);
-	printf(" while NOT (~) of mask right shifted to index %d is ", index);
-	print_bin(mask);
-	printf(" our new value n in binary is ");
-	print_bin(*n);
-	printf("\n\n");
-	#endif
-
-	return (1);
+	return (nbits);
 }
